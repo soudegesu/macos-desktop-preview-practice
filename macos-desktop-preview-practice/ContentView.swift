b3dc580d4fa4831b6bb133003749dfa464110015
soudegesu <soudegesu@hotmail.co.jp>
2021-03-22 16:44:40 +0900

    initial commit

---
+++ macos-desktop-preview-practice/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  macos-desktop-preview-practice
+//
+//  Created by Takaaki Suzuki on 2021/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
